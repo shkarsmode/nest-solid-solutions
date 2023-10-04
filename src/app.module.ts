089cf4f204b949e1fc389162ff11162de3b2d066
Shkarupa Daniil <zshkarrr@outlook.com>
2023-10-04 17:38:15 +0300

    #3 Add supprot mysql + set up migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '',
+            database: "blog",
+            entities: ["dist/**/*.entity{.ts,.js}"],
+            dropSchema: false,
+            synchronize: true,
+            migrationsRun: false,
+            logging: true,
+            migrations: ["dist/**/db/migrations/*{.ts,.js}"]
+        })
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
